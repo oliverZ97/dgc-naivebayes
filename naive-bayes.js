0d2a3887a89610a3547422041ffed031cc937a9e
OliverZ97 <oliver@mordoor.de>
2019-12-06 17:20:51 +0100

    initial commit

---
+++ naive-bayes.js
@@ -0,0 +1,10 @@
+const bayes = require('bayes');
+const bayesClass = bayes();
+
+//csv => articleid, realCat, computedCat, Match
+module.exports = class naivebayes {
+    
+    getContentOfArticle() {
+    
+    }
+}
