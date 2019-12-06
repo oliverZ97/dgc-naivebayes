9579e37c592e4a3616c4bdb941e35f4da561578e
OliverZ97 <oliver@mordoor.de>
2019-12-06 17:29:35 +0100

    read in files and extract articles

--- naive-bayes.js
+++ naive-bayes.js
@@ -1,10 +1,45 @@
-const bayes = require('bayes');
-const bayesClass = bayes();
+var bayes = require('bayes');
+var bayesClass = bayes();
+const fs = require("fs");
+const striphtml = require("string-strip-html")
 
 //csv => articleid, realCat, computedCat, Match
-module.exports = class naivebayes {
-    
-    getContentOfArticle() {
-    
-    }
+
+function parseJSONFile() {
+    let filedir = fs.readdirSync("./data");
+    filedir.forEach((file) => {
+        let filestring = fs.readFileSync("./data/" + file);
+        let fileobj = JSON.parse(filestring);
+        //console.log(fileobj);
+        extractDocuments(fileobj);
+    })
+    //saveModel()
 }
+
+function extractDocuments(jsonobj) {
+    let documents = jsonobj.documents;
+    let testset = extractTestData(documents)
+    // documents.forEach(element => {
+    //     let text = element.content;
+    //     console.log(text);
+    //     let stripText = striphtml(text);
+    //     //teachModel(stripText, element.category)
+    // });
+}
+
+function extractTestData(documents) {
+    let testdata = JSON.stringify(documents.slice(0, 199));
+    console.log(testdata);
+    fs.writeFileSync("./data/test.json", testdata);
+
+}
+
+function teachModel(text, category) {
+    bayesClass.learn(text, category);
+}
+
+function saveModel() {
+    var model = bayesClass.toJson();
+    console.log()
+}
+parseJSONFile()
